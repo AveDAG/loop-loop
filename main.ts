8b9fd7d6741c4826378c2c488a1a0c6538e8722b
AveDAG <58709240+AveDAG@users.noreply.github.com>
2020-01-31 13:18:21 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+// 1. create two new variables that makes a for loop with a for loop increments up from 1 to 10.
+// The for loop must use the two variables.
+
+// 2. create two new variables with a for a loop that counts from 11 to 20
+// The for loop must use the two variables.
+
+// 3. create two new variables with a for loop that counts down from 5 to 0
+// The for loop must use the two variables.
+
+// 4. create two new variables for loop to make a countdown from 100 to 0, skiping 10 numbers each time
+// The for loop must use the two variables.
+
